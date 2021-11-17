1cab5e86130522280c1965630b707428a9aa9885
twoodard3344 <62488194+twoodard3344@users.noreply.github.com>
2021-11-16 19:11:15 -0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,36 @@
+// To run this assignment, right click on index.html in the Visual Studio file explorer to the left
+// and select "Open with Live Server"
+
+// Create your own Madlibs story!
+
+/*let animal = prompt("Name a kind of animal");
+let place = prompt("Name a place");
+let food = prompt("Name a food");
+document.write("Here is a story for you!<br>");
+document.write(
+  "The inconsiderate " +
+    animal +
+    " strode proudly through " +
+    place +
+    ", dropping " +
+    food +
+    " everywhere."
+);
+
+*/
+
+let adjectiveOne = prompt("Name an adjective");
+let nationality = prompt("Name a nationality");
+let person = prompt("Name a person");
+let foodOne = prompt("Name a food");
+let foodTwo = prompt("Name another food");
+let numberOf = prompt("Name a number");
+document.write(
+  "Pizza was created by " + adjectiveOne + " "+
+    nationality + " chef named " + person +
+  ". Some people like " + foodOne +
+   " pizza the best, but most people like the " + foodTwo +
+   " pizza. 45% of Americans eat " + numberOf + " pizzas per month.");
+
+  
+
